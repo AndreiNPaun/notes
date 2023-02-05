4c4a772eb4476e22ce9b1a6156a10efac6019a5a
Andrei <andreinicolaepaun@gmail.com>
2023-02-05 17:39:42 +0000

    removed unnecessary client files

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
