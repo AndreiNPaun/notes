03a3a516e21d390df53ffa7c91ad7891d7e5267c
Andrei <andreinicolaepaun@gmail.com>
2023-02-08 14:19:12 +0000

    feature implemented

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Login from './Authentication/Login';
 import Register from './Authentication/Register';
+import Card from './UI/Card';
+
+import classes from './Homepage.module.css';
+import Button from './UI/Button';
 
 const Homepage = () => {
+  const [registerButton, setRegisterButton] = useState(false);
+  const [loginButton, setLoginButton] = useState(false);
+
+  const registerForm = () => {
+    setRegisterButton(true);
+  };
+
+  const loginForm = () => {
+    setLoginButton(true);
+  };
+
+  console.log(`register ${registerButton} login ${loginButton}`);
+
   return (
     <React.Fragment>
-      <Register />
-      <Login />
+      {!registerButton && !loginButton && (
+        <Card className={classes.form}>
+          <div className={classes.center}>
+            <Button type="submit" onClick={registerForm}>
+              Register
+            </Button>
+          </div>
+          <div className={classes.center}>
+            <Button type="submit" onClick={loginForm}>
+              Login
+            </Button>
+          </div>
+        </Card>
+      )}
+      {registerButton && <Register />}
+      {loginButton && <Login />}
     </React.Fragment>
   );
 };
