0853e2fd71407a410d9b1efb186f2bfb80954f44
Andrei <andreinicolaepaun@gmail.com>
2023-05-13 17:04:14 +0100

    express-validator and validation logic added

--- server/Routes/Authenticate.js
+++ server/Routes/Authenticate.js
@@ -1,11 +1,59 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const { body } = require('express-validator');
+
+const User = require('../Models/User');
+const bcrypt = require('bcryptjs');
 
 const AuthController = require('../Controllers/AuthController');
 
-router.post('/register', AuthController.register);
-router.post('/login', AuthController.login);
+router.post(
+  '/register',
+  [
+    body('email', 'Invalid email format.')
+      .isEmail()
+      .custom(async (email, { req }) => {
+        const userData = await User.findOne({ email });
+        if (userData) {
+          throw new Error('E-mail address already exists.');
+        }
+      }),
+    body('password', 'Invalid password.')
+      .isLength({ min: 5 })
+      .custom((password, { req }) => {
+        if (password !== req.body.confirmPassword) {
+          throw new Error('Passwords have to match.');
+        }
+        return true;
+      }),
+  ],
+  AuthController.register
+);
+router.post(
+  '/login',
+  [
+    body('email', 'Wrong email.')
+      .isEmail()
+      .custom(async (email, { req }) => {
+        const userData = await User.findOne({ email });
+        if (!userData) {
+          throw new Error('User not found.');
+        }
+      }),
+    body('password', 'Wrong password.')
+      .trim()
+      .notEmpty()
+      .custom(async (password, { req }) => {
+        const userData = await User.findOne({ email: req.body.email });
+        const passwordMatch = await bcrypt.compare(password, userData.password);
+        if (!passwordMatch) {
+          throw new Error('Wrong password.');
+        }
+      }),
+  ],
+  AuthController.login
+);
 router.post('/refresh-token', AuthController.refreshToken);
 
 // Google
