1a851d5709344bf0b5d325eca182bee0867320b8
Andrei <andreinicolaepaun@gmail.com>
2023-05-25 20:53:26 +0100

    frontend validation added and authentication logic changed

--- client/src/hooks/useHttp.js
+++ client/src/hooks/useHttp.js
@@ -4,12 +4,15 @@ const useHttp = async ({ method = 'get', url, values, token }) => {
   const options = {
     headers: {
       'Content-type': 'application/json',
-      Authorization: `Bearer ${token}`,
     },
   };
 
   if (method !== 'get') {
     options.data = values;
+  }
+
+  if (token) {
+    options.headers.Authorization = `Bearer ${token}`;
   }
 
   const response = await axios({ method, url, ...options });
